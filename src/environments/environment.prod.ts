67b74ab0d82b75ea1f4c068d0371a4e0d022ca8e
OSTROWSKI Arkadiusz <arkadiusz.ostrowski2@soprasteria.com>
2023-02-01 23:11:14 +0100

    environment file replace

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   firebaseAPIKey: 'AIzaSyDjsWnXDV89SsKqpoSOGpf4borDjzrQGZM',
+  databaseUrl:
+    'https://cinemareservation-86c0d-default-rtdb.firebaseio.com/movies.json',
 };
